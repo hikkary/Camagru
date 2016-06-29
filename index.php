f5b80e8845b9c881c70f6aa39af5492e56dd540a
Zakaria KERKEB <zkerkeb@e3r9p16.42.fr>
2016-06-29 21:15:39 +0200

    regler la page d'acceuil, je ne vous demande pas de me suivre, mais vous serez mes temoins

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/index.css">
+	<title>Camagru</title>
+</head>
+<body>
+<div id="logo">
+<img src="img/logo.png">
+</div>
+<div id="polaroid">
+<img src="img/Polaroid400.png">
+<div id="inscription">
+<form  action="#" method="post">
+<!-- <fieldset> -->
+<h1>INSCRIPTION</h1>
+<!-- <label for="email">Email</label> -->
+<input id="email" type="email" placeholder="Email">
+
+<!-- <label for="email">Nom d'utilisateur</label> -->
+<input id="username" type="text" placeholder="Nom d'utilisateur">
+
+<!-- <label for="password">Mot de passe</label> -->
+<input id="password" type="password" placeholder="Mot de passe">
+
+<input id="sub" type="submit" name="s'inscrire">
+<!-- </fieldset> -->
+</form>
+</div>
+</div>
+
+
+</body>
+</html>
