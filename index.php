1fda66f282f40b0ac8c9c0d827621a3977d50c23
hikkary <z.kerkeb@gmail.com>
2016-07-24 07:48:44 +0200

    Tout le monde meurt, mais certains on besoin d'aide

--- index.php
+++ index.php
@@ -3,35 +3,33 @@
 <head>
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/index.css">
+	<meta name="viewport" content="width=device-width, user-scalable=yes" />
 	<title>Camagru</title>
 </head>
 <body>
 <div id="header">
-<img src="img/headerfp.png" width="100%">
+<img src="img/top.png" width="100%" >
 </div>	
-
+<!-- 
 <div id="logo">
 <img src="img/logo.png">
+</div> -->
+<!-- <div id="polaroid">
+<img src="img/Polaroid400.png"> -->
+<div id="hero">
+<img src="img/inscription.png" width="100%">
 </div>
-<div id="polaroid">
-<img src="img/Polaroid400.png">
+
 <div id="inscription">
 <form  action="#" method="post">
-<!-- <fieldset> -->
-<h1>INSCRIPTION</h1>
-<!-- <label for="email">Email</label> -->
 <input id="email" type="email" placeholder="Email">
-
-<!-- <label for="email">Nom d'utilisateur</label> -->
 <input id="username" type="text" placeholder="Nom d'utilisateur">
-
-<!-- <label for="password">Mot de passe</label> -->
 <input id="password" type="password" placeholder="Mot de passe">
-
 <input id="sub" type="submit" name="s'inscrire">
-<!-- </fieldset> -->
-</form>
+<div id="footer">
+<img src="img/yjuni.png">
 </div>
+</form>
 </div>
 
 
