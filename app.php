35453861c5448b0087ac6e750b4e582bdabd6208
hikkary <z.kerkeb@gmail.com>
2016-07-31 21:53:06 +0200

    Sa me rappelle la chasse aux oies

--- app.php
+++ app.php
@@ -10,7 +10,8 @@
 
 <div id = top-app>
 <img src="img/logo.png" >
-<i class="fa fa-times" aria-hidden="true"></i>
+<i id="croix" class="fa fa-times" aria-hidden="true" title="Deconnexion"></i>
+<i id="album" class="fa fa-picture-o" aria-hidden="true" title="Album"></i>
 <!-- <p>Deconnexion</p> -->
 </img>	
 </div>
@@ -19,13 +20,18 @@
 <!-- <img src="img/gcam.png"> -->
 <video id="video"></video>
 <img src="img/nphoto.png" id="photo" alt="photo">
+<button id="startbutton">
+<i id="camera" class="fa fa-camera" aria-hidden="true"></i>
+</button>
+<button id="upload">
+<i class="fa fa-arrow-up" aria-hidden="true"></i>
+</button>
 </div>
 </div>
-<button id="startbutton">Hiro ni naritai</button>
-
-<canvas id="canvas">
-
-</canvas>
+<canvas id="canvas"></canvas>
+<div id="montage">
+<p>Ace</p>
+</div>
 </body>
 	<script type="text/javascript" src="javascript/videome.js"></script>
 </html>
