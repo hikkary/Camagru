2b4931b4e8585bfb36fc8b3ca696e1c21902d17c
hikkary <z.kerkeb@gmail.com>
2016-09-05 05:17:38 +0200

    Terminer recuperation mot de passe, voir Trello pour detail, l'aube se leve

--- forgot.php
+++ forgot.php
@@ -6,7 +6,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/forgot.css">
 	<link rel="stylesheet" href="css/menu.css">
 	<meta name="viewport" content="width=device-width, user-scalable=yes" />
 	<link rel="stylesheet" href="fonts/css/font-awesome.min.css">
@@ -24,18 +24,18 @@
 </div> -->
 
 <div id="redirect">
-<p id="message"> Login </p>
+<p id="message"> Please enter your Username or Email </p>
 <form id="decathlon" method="post">
 		<input id="username" type="text" placeholder="Username or Email">
 		<!-- <input id="password" type="password" placeholder="Type Password"> -->
 		<div id="submit">
 		<a href="#"><p>Submit</p>
 		</div></a>
-		<a id="forgot" href="forgot.php">I forgot my password</a>
+		<!-- <a id="forgot" href="forgot.php">I forgot my password</a> -->
 		 <!-- id="sub" type="submit" name="s'inscrire"> -->
 	</form>
 </div>
 
 </body>
-<script type="text/javascript" src="javascript/login.js"></script>
+<script type="text/javascript" src="javascript/forgot.js"></script>
 </html>
---
+++ javascript/forgot.js
@@ -0,0 +1,54 @@
+ var username = document.getElementById('username');
+ // var password = document.getElementById('password');
+ // var submit = document.getElementById('submit');
+ var message = document.getElementById('message');
+
+ function changetext(message,element,color,original_message)
+ {
+ 	element.innerHTML = message;
+ 	element.style.color = color;
+ 	if(original_message)
+ 	{
+ 		setTimeout(function(){
+ 			changetext(original_message,element,"black", null);
+ 		},3000);
+ 	}
+ }
+
+function connect(username,password){
+  var error = "Wrong Username or Password";
+  var connect = new XMLHttpRequest();
+	connect.onreadystatechange = function(){
+		if (connect.readyState == 4 && connect.status == 200)
+		{
+			const bool = JSON.parse(connect.responseText);
+			console.log(bool);
+			if(bool == "false")
+				window.location.href = "app.php";
+			else
+      {
+				changetext(error,document.getElementById('message'),"red", null);
+      }
+		}
+	};
+	const data = {
+		t_username : username,
+		t_password : password
+	};
+	connect.open("POST","ajax/connect.php",true);
+    connect.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	connect.send(JSON.stringify(data));
+}
+
+
+
+submit.addEventListener('click',function(ev){
+	var formfill = "Please write Username or Email";
+	if(username.value.length === 0)
+	{
+		changetext(formfill,message,"red",message.innerHTML);
+		return;
+	}
+	else
+		connect(username.value,password.value);
+},true);
